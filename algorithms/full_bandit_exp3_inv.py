597162b2ef6625c0d5e6e5eff005e0e26ecc2826
LukasZierahn <lukas@kappa-mm.de>
2022-09-15 13:41:53 +0200

    trying inverse

---
+++ algorithms/full_bandit_exp3_inv.py
@@ -0,0 +1,30 @@
+from functools import partial
+import numpy as np
+
+from algorithms.algorithm import Algorithm
+from algorithms.full_bandit_exp3 import FullBanditExp3
+from distributions.sequence import Sequence
+from misc.matrix_geometric_resampling import matrix_geometric_resampling
+
+class FullBanditExp3Inv(FullBanditExp3):
+
+    def observe_loss(self, loss: float, context: np.ndarray, action_index: int):
+
+        K = self.actionset.K
+        d = len(context)
+
+        action_matrix = np.zeros((K, K))
+        for i in range(d):
+            context = np.zeros(d)
+            context[i] = 1
+
+            probabilities = self.get_policy(context)
+            weighted_action = np.einsum("ab,a->b", self.actionset.actionset, probabilities)
+            action_matrix += np.outer(weighted_action, weighted_action) / d
+
+        tensor = np.einsum("ab,cd->cadb", np.identity(d)/d,  action_matrix).reshape((d*K, d*K))
+        inverse = np.linalg.inv(tensor)
+
+        self.theta_estimates[self.theta_position] = loss * np.einsum("abcd,b,c", inverse, context, self.actionset[action_index])
+        self.theta_position += 1 
+
